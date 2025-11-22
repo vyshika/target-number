b102329f84d58ea971b005b175fa3c0edefc46da
Vaishika V <vaishika14072004@gmail.com>
2025-11-22 11:48:49 +0530

    updated code

--- test2.py
+++ test2.py
@@ -1,18 +1,16 @@
-import random
-num=[0,1,3,2,6,8,7,4,5,10,9]
-target=int(input("enter the target number below 10: "))
-while True:
-    num1=random.randint(num[0],num[10])
-    num2=random.randint(num[0],num[10])
-    c=num1+num2
-    ind1=num1
-    ind2=num2
-    if c==target:
-        print("index is","[",num.index(ind1),",",num.index(ind2),"]","in list",num)
-        print(f"{num1} + {num2} = {c}")
-        break 
-    elif c!=target:
-        continue
-    break
+num=(input("enter a list of numbers")).split()
+num=[int(x) for x in num]
+print(num)
+target=int(input("target number:"))
+i=0
+while i < (len(num)):
+    j=i+1
+    while j < (len(num)):
+        result=num[i]+num[j]
+        if result == target:
+            print(num[i] ,"+",num[j])
+            print("index :",i,j)
+        j+=1
+    i+=1
 
 
